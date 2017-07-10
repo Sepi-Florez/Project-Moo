b51c9778ba3c506e3c76aa198e1eb657a1680e25
David V <david.vink@hotmail.com>
2017-07-10 10:34:30 +0200

    UML work and created the unity project

---
+++ Project Moo/Assets/Scripts/Grid/FarmGrid.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmGrid : MonoBehaviour {
+    public static int w;
+    public static int h;
+    public static Transform[,] grid = new Transform[w,h];
+}
